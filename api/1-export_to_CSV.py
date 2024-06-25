b6bcc5c219f5d8cf378424cdfcb2518d283359a5
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-06-25 11:09:45 +0200

    ft: exporting user and tasks info in a csv file

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+import requests
+import sys
+import os
+
+def get_user_info(userId):
+    userInfoUrl = 'https://jsonplaceholder.typicode.com/users/{userId}'.format(userId=userId)
+    userTasksInfoUrl = 'https://jsonplaceholder.typicode.com/users/{userId}/todos'.format(userId=userId)
+
+    try:
+        # Fetching user 
+        userInfoResponse = requests.get(userInfoUrl)
+        userInfoResponse.raise_for_status()
+
+        # Fetching user tasks
+
+        userTasksInfoResponse = requests.get(userTasksInfoUrl)
+        userTasksInfoResponse.raise_for_status()
+
+        if userInfoResponse.status_code == 200 and userTasksInfoResponse.status_code == 200:
+            user = userInfoResponse.json() 
+            tasks = userTasksInfoResponse.json()
+            
+            # Calculate Tasks Progress
+            total_tasks = len(tasks)
+
+
+            # Prepare CSV file
+            file = f"{userId}.csv"
+            fieldNames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+            with open(file, mode='w') as csv_file:
+                csv_file.write(','.join(fieldNames) + '\n')
+                for task in tasks:
+                    csv_file.write(f"{userId},{user['username']},{task['completed']},{task['title']}\n")
+
+            print(f"User info and tasks have been exported to {csv_file}")            
+
+        else: 
+            print(f"Unexpected status code: {response.status_code}")
+
+    except requests.exceptions.HTTPError as e:
+        print(f"Error: {e}")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: {} <employeeId>".format(sys.argv[0]))
+        sys.exit(1)
+    else:
+        userId = sys.argv[1]
+        get_user_info(userId)
